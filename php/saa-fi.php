31ac5758658f11e6a12bf1ae8102e7ea3a727aff
Lauri Jakku A <lja@lja.fi>
2021-05-28 00:38:44 +0300

    synkki

--- php/saa-fi.php
+++ php/saa-fi.php
@@ -1,28 +1,39 @@
 <html>
-<body>
 <?php
 
 /* Login details */
 include("login.inc");
-/* Login details 
-	global $GLOBAL_hostname;
-	global $GLOBAL_username;
-	global $GLOBAL_password;
-	global $GLOBAL_weatherinfotable;
-*/
 
+
+function weather_get_weather_info_table_name()
+{
+	return $GLOBALS['mysql_weatherinfotable'];
+}
 
 function weather_do_mysql_query($query)
 {
+	$q = $query . ";";
+	echo "Query: $q";
 
-	$mysqli = new mysqli($GLOBAL_hostname, $GLOBAL_username, $GLOBAL_password, $GLOBAL_weatherinfotable);
+	$mysqli = new mysqli(	$GLOBALS['mysql_hostname'],
+				$GLOBALS['mysql_username'],
+				$GLOBALS['mysql_password'],
+				$GLOBALS['mysql_databasename']);
 
 	// Check connection
 	if ($mysqli->connect_errno) {
-		echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+		echo "Failed to connect to MySQL: " . $mysqli->connect_error;
 	}
 
-	$result = $mysqli -> query($query);
+	$result = $mysqli -> query($q);
+
+	// Check connection
+	if ($mysqli->connect_errno) {
+		echo "Failed to request from MySQL: " . $mysqli->connect_error;
+	}
+
+	echo "<br>Query : $q";
+	echo "<br>Result: $result<br>";
 
 	$mysqli -> close();
 
@@ -31,17 +42,20 @@ function weather_do_mysql_query($query)
 
 function weather_get_mysql_data_last_record_number()
 {
-	$q = "select * from $GLOBAL_weatherinfotable";	
+	$q = "select max(rec_nro) from " . weather_get_weather_info_table_name();	
 
-	$r = weather_do_mysql_query($q);
+	$res = weather_do_mysql_query($q);
 
-	print($r);
+	return $res;
 }
 
-echo weather_get_mysql_data_last_record_number();
+$r = weather_get_mysql_data_last_record_number();
+
+echo "JEES poks";
+
+print_r($r);
 
 ?>
-</body>
 </html>
 
 
