703e3cbbe54b41f972aafad68c28c39de4a50931
Lauri Jakku A <lja@lja.fi>
2021-06-05 02:24:40 +0300

    evaluationi

--- finnish-weather-wp-plugin.php
+++ finnish-weather-wp-plugin.php
@@ -20,13 +20,22 @@
 	/* MYSQL stuff */
 	include_once ($FINWEATHER_PLUGIN_DIR . "php/mysql.inc");
 
+	function evaluate_file($f)
+	{
+		ob_start();
+			include($f);
+			$content = ob_get_clean();
+			
+		ob_start();
+		return $content;
+	}
 
 	// This just echoes the chosen line, we'll position it later.
 	function finnish_weather_wp_plugin_header()
 	{
 		global $FINWEATHER_PLUGIN_DIR;
 
-		echo file_get_contents($FINWEATHER_PLUGIN_DIR . "php/header.php"); 
+		echo evaluate_file($FINWEATHER_PLUGIN_DIR . "php/header.php"); 
 	}
 
 	// This just echoes the chosen line, we'll position it later.
@@ -35,7 +44,7 @@
 		global $FINWEATHER_PLUGIN_DIR;
 
 //		echo '<meta startti="lja" />\n';
-//		include_once($FINWEATHER_PLUGIN_DIR . "php/body.php"); 
+//		evaluate_file($FINWEATHER_PLUGIN_DIR . "php/body.php"); 
 //		echo '<meta stoppi="lja" />\n';
 
 	}
@@ -46,7 +55,7 @@
 		global $FINWEATHER_PLUGIN_DIR;
 
 		echo '<meta shortti="lja alkaa" />';
-		echo file_get_contents($FINWEATHER_PLUGIN_DIR . "php/body.php"); 
+		echo evaluate_file($FINWEATHER_PLUGIN_DIR . "php/body.php"); 
 		echo '<meta shortti="lja stoppaa" />';
 	}
 
