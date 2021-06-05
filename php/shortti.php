ce92956a3dad2dea891f89a412c8abea1f85370d
Lauri Jakku A <lja@lja.fi>
2021-06-05 14:58:21 +0300

    shortti listätty

---
+++ php/shortti.php
@@ -0,0 +1,109 @@
+<?php
+
+	/* Login details */
+	include_once("login.inc");
+
+	/* SQL details */
+	include_once("mysql.inc");	
+
+	function mkMarker($obj)
+	{
+		$jsn = json_encode($obj);
+		$quot= "'";
+
+		$stout = "makeMarker(myMap," . $quot . $jsn . $quot . ");";
+
+		echo $stout . "\n";
+	}
+
+	function comment($txt)
+	{
+
+	}
+
+	$LatestID = weather_get_mysql_data_last_record_number();
+
+	$stations = weather_get_mysql_record_number_datas($LatestID, "stationname ASC");
+
+	//print_r($stations);
+
+	function generate_markers($stations)
+	{
+
+/*		
+		echo "Stations:";
+		print_r($stations);
+		echo "<br>";
+		comment("Stations got:");
+		echo "<!---\n";
+		print_r($_GET['stations']);
+		echo "--->\n";
+
+*/
+
+		if ( $fv = $_GET['stations'] )
+		{
+			foreach ($fv as $selectedStation) {
+				$stationData = $stations[$selectedStation];
+			/*
+				comment("Marker $selectedStation");
+				echo "<!---\n";
+				print_r($stationData);
+				echo "--->\n";
+			*/
+				mkMarker($stationData);
+			}
+		}
+	}
+
+	function generate_station_selectiors($stations) {
+		$FID = 0;
+		$htmlEndln = "<br>\n";
+		foreach ($stations as $stationi) {
+			$station = $stationi[1];
+			$htmlIP = "<option value=";
+			$htmlIP = $htmlIP . $FID . ">";
+			$htmlIP = $htmlIP . $station;
+			$htmlIP = $htmlIP . "</option>";
+			echo $htmlIP . $htmlEndln;
+			$FID++;
+		}
+	}
+
+	include("body_selection_generate.inc");
+
+
+?>
+
+	<table>
+		<tr>
+			<td>
+
+				<form action="<?php echo $SELF; ?>" method="get">
+					<label for="stations">Valitse sääasemat</label>
+					<br>
+					<select name="stations[]" id="stations" multiple size="10">
+					<br>
+					
+					<?php generate_station_selectiors($stations); ?>
+
+					</select>
+					<br>
+					<input type="submit" value="Valitse">
+				</form>
+			</td>
+			<td>
+				    <div id="osmMap" style="width:450px;height:550px;">
+						<div id="osmPop" style="visibility: visible; display: block;">
+							<a href="#" id="osmPop-closer"></a>
+							<div id="osmPop-content" style="visibility: visible; display: block;"></div>
+						</div>
+					</div> 
+
+					<script>
+						window.document.onload = cb_onLoadDocument(event, cb_after);
+					</script>
+
+			</td>
+		</tr>
+	</table>
