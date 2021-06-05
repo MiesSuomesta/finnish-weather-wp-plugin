07e1f1a036b71f9b189ada5ef2cb2fe97b388096
Lauri Jakku A <lja@lja.fi>
2021-06-05 15:01:45 +0300

    body class actionilla toimiikos

--- finnish-weather-wp-plugin.php
+++ finnish-weather-wp-plugin.php
@@ -25,8 +25,8 @@
 		ob_start();
 			include($f);
 			$content = ob_get_clean();
-			
 		ob_start();
+
 		return $content;
 	}
 
@@ -61,7 +61,7 @@
 
 	// Now we set that function up to execute when the admin_notices action is called.
 //	add_action( 'wp_head', 			'finnish_weather_wp_plugin_header' 	);
-//	add_action( 'wp_body_open',		'finnish_weather_wp_plugin_body' 	);
+	add_action( 'body_class',		'finnish_weather_wp_plugin_body' 	);
 
 	add_shortcode('finweather', 'finnish_weather_wp_plugin_shortcode'	);
 
