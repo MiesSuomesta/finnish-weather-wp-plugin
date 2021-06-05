87099deeb61cf903d54b07fa6b61108be6e795e6
Lauri Jakku A <lja@lja.fi>
2021-06-05 14:55:25 +0300

    open action body

--- finnish-weather-wp-plugin.php
+++ finnish-weather-wp-plugin.php
@@ -61,7 +61,7 @@
 
 	// Now we set that function up to execute when the admin_notices action is called.
 	add_action( 'wp_head', 			'finnish_weather_wp_plugin_header' 	);
-//	add_action( 'wp_body_open',		'finnish_weather_wp_plugin_body' 	);
+	add_action( 'wp_body_open',		'finnish_weather_wp_plugin_body' 	);
 
 	add_shortcode('finweather', 'finnish_weather_wp_plugin_shortcode'	);
 
