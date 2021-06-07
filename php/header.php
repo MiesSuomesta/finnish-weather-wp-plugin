702800b2e9c4ed122a3d49b76bb5bbf94f18e4ae
Lauri Jakku A <lja@lja.fi>
2021-06-07 06:49:21 +0300

    yhistämistä korjailtu 2

--- php/header.php
+++ php/header.php
@@ -5,7 +5,5 @@
 	<link rel="stylesheet" href="https://openlayers.org/en/v4.6.5/css/ol.css" type="text/css">
 	<script src="https://openlayers.org/en/v4.6.5/build/ol.js" type="text/javascript"></script>
 
-	<?php require_once('body_selection_generate.inc'); ?>
-
 	<meta headeri="lja stoppaa" />
 	<!--- LJA HEADER END --->
