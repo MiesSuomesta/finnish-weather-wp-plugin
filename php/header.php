505cd1e99b485c64a3d38f9d6629635e7b0242cc
Lauri Jakku A <lja@lja.fi>
2021-06-07 08:51:01 +0300

    lisäsin headeriin functio incin

--- php/header.php
+++ php/header.php
@@ -3,5 +3,7 @@
 	<link rel="stylesheet" href="https://openlayers.org/en/v4.6.5/css/ol.css" type="text/css">
 	<script src="https://openlayers.org/en/v4.6.5/build/ol.js" type="text/javascript"></script>
 
+	<?php include_once('header-functions.inc'); ?>
+	
 	<?php include_once('body_selection_generate.inc'); ?>
 	<!--- LJA HEADER END --->
