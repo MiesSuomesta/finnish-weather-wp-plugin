5c51e2513584dc51d28d1b4779f33d97b04dfc01
Lauri Jakku A <lja@lja.fi>
2021-06-07 06:51:40 +0300

    yhistämistä korjailtu 3

--- php/header.php
+++ php/header.php
@@ -4,6 +4,8 @@
 	
 	<link rel="stylesheet" href="https://openlayers.org/en/v4.6.5/css/ol.css" type="text/css">
 	<script src="https://openlayers.org/en/v4.6.5/build/ol.js" type="text/javascript"></script>
-
+	
+	<?php require_once("header-functions.inc"); ?>
+	
 	<meta headeri="lja stoppaa" />
 	<!--- LJA HEADER END --->
