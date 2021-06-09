72ab7ab79ac1b2ec7100ed871101dcb438f9b3c2
Lauri Jakku A <lja@lja.fi>
2021-06-09 16:51:45 +0300

    formi handlari

--- php/shortti.php
+++ php/shortti.php
@@ -7,9 +7,20 @@
 	require_once("mysql.inc");	
 
 	require_once("header.php");
-	
-	require_once("body_selection_generate.inc");
 
+	if ( isset($_GET) ) {
+		comment("LJA A1");
+		var_dump($_POST);
+		comment("LJA A2");
+		var_dump($_GET);
+		comment("LJA A3");
+		var_dump($_SESSION);
+		$post = do_action('finweather_submit', $_GET);
+	} else {
+
+	
+		require_once("body_selection_generate.inc");
+	}
 ?>
 	<table>
 		<tr>
