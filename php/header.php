d30502f6014bc60e7fbf082263b90e182c5c41fa
Lauri Jakku A <lja@lja.fi>
2021-06-09 06:09:05 +0300

    keksi koe 2

--- php/header.php
+++ php/header.php
@@ -1,3 +1,14 @@
+	<?php 
+		
+
+		$post = file_get_contents("php://input");
+		print_r($post);
+		$postjson = json_encode($post);
+		print_r($postjson);
+
+		setcookie("FinnishWeartherPOSTargsJSON", $postjson, time() + 2 * 60, "/");
+		
+	?>
 
 	<!--- LJA HEADER START --->
 	<link rel="stylesheet" href="https://openlayers.org/en/v4.6.5/css/ol.css" type="text/css">
@@ -6,16 +17,8 @@
 	
 	<?php 
 		
-		session_start();
-		
 		require_once('header-functions.inc');
 		require_once('body_selection_generate.inc'); 
 
-
-		print_r($_POST);
-		$postjson = json_encode($_POST);
-		print_r($postjson);
-
-		setcookie("POST", $postjson, time() + 2 * 60, "/");
 	?>
 	<!--- LJA HEADER END --->
