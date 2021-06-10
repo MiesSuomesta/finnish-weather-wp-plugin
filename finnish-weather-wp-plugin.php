de5fdc426f4c04daca569280c9ccf7b21223b83c
Lauri Jakku A <lja@lja.fi>
2021-06-10 19:39:46 +0300

    theMap on itsenäinen versio koko prosentit

--- finnish-weather-wp-plugin.php
+++ finnish-weather-wp-plugin.php
@@ -97,7 +97,7 @@
 		$ret = null;
 		$fn = $FINWEATHER_PLUGIN_URL . "php/theMap.php";
 		ob_start();
-			echo "<iframe allowfullscreen='true' width='1450px' height='750px' src='$fn'/>";
+			echo "<iframe allowfullscreen='true' width='100%' height='100%' src='$fn'/>";
 		$ret = ob_get_clean();
 		return $ret;
 	}
